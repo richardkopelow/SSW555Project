45ba743d56430f527b87d350b1cedc700bae60b3
Richard Kopelow <richardkopelow@gmail.com>
2018-08-02 20:09:02 -0400

    Error is printed if an indevidual is older than 150 years

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, ParseException
-    {
+    public static void main(String[] args) throws IOException, ParseException {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the path for the config file:");
         String configPath = scan.nextLine();
@@ -27,26 +26,20 @@ public class Main {
         validTags[2] = configFile.readLine().split(",");
 
         ArrayList<Entry> entries = new ArrayList<Entry>();
-        ArrayList<GEDError> gedErrors =  new ArrayList<GEDError>();
+        ArrayList<GEDError> gedErrors = new ArrayList<GEDError>();
 
         int count = 1;
-        //Parse out the ged file
+        // Parse out the ged file
         String line = gedFile.readLine();
-        while(line!=null)
-        {
+        while (line != null) {
             Entry entry = new Entry(line, count);
-            try
-            {
+            try {
                 if (IsValid(entry.Tag, validTags[entry.Level])) {
                     entries.add(entry);
-                }
-                else
-                {
+                } else {
                     gedErrors.add(new EntryError(count, entry.Tag, entry.Level));
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 gedErrors.add(new EntryError(count, entry.Tag, entry.Level));
             }
 
@@ -57,7 +50,7 @@ public class Main {
         configFile.close();
         gedFile.close();
 
-        //Develop Family map
+        // Develop Family map
         HashMap<String, Family> families = new HashMap<String, Family>();
         HashMap<String, Person> people = new HashMap<String, Person>();
         Family workingFamily = new Family(people);
@@ -66,84 +59,84 @@ public class Main {
 
         for (int i = 0; i < entries.size(); i++) {
             Entry ent = entries.get(i);
-            try
-            {
+            try {
                 switch (ent.Tag.toLowerCase()) {
-                    case "indi":
-                        workingPerson = new Person();
-                        workingPerson.ID = ent.Args;
-                        people.put(ent.Args, workingPerson);
-                        break;
-                    case "fam":
-                        workingFamily = new Family(people);
-                        workingFamily.ID = ent.Args;
-                        families.put(ent.Args, workingFamily);
-                        break;
-                    case "name":
-                        workingPerson.Name = ent.Args;
-                        break;
-                    case "sex":
-                        workingPerson.Gender = ent.Args;
-                        break;
+                case "indi":
+                    workingPerson = new Person();
+                    workingPerson.ID = ent.Args;
+                    people.put(ent.Args, workingPerson);
+                    break;
+                case "fam":
+                    workingFamily = new Family(people);
+                    workingFamily.ID = ent.Args;
+                    families.put(ent.Args, workingFamily);
+                    break;
+                case "name":
+                    workingPerson.Name = ent.Args;
+                    break;
+                case "sex":
+                    workingPerson.Gender = ent.Args;
+                    break;
+                case "birt":
+                case "deat":
+                case "marr":
+                case "div":
+                    stage2Type = ent.Tag;
+                    break;
+                case "famc":
+                    workingPerson.Child = ent.Args;
+                    break;
+                case "fams":
+                    workingPerson.Spouse = ent.Args;
+                    break;
+                case "wife":
+                    workingFamily.WifeID = ent.Args;
+                    break;
+                case "husb":
+                    workingFamily.HusbandID = ent.Args;
+                    break;
+                case "chil":
+                    workingFamily.Children.add(ent.Args);
+                    break;
+                case "date":
+                    DateFormat fmt = new SimpleDateFormat("dd MMM yyyy");
+                    Date date = fmt.parse(ent.Args);
+                    switch (stage2Type.toLowerCase()) {
                     case "birt":
+                        workingPerson.SetBirthday(date);
+                        break;
                     case "deat":
+                        workingPerson.SetDeathday(date);
+                        break;
                     case "marr":
+                        workingFamily.SetMarrageDate(date);
+                        break;
                     case "div":
-                        stage2Type = ent.Tag;
+                        workingFamily.DivorceDate = date;
                         break;
-                    case "famc":
-                        workingPerson.Child = ent.Args;
-                        break;
-                    case "fams":
-                        workingPerson.Spouse = ent.Args;
-                        break;
-                    case "wife":
-                        workingFamily.WifeID = ent.Args;
-                        break;
-                    case "husb":
-                        workingFamily.HusbandID = ent.Args;
-                        break;
-                    case "chil":
-                        workingFamily.Children.add(ent.Args);
-                        break;
-                    case "date":
-                        DateFormat fmt = new SimpleDateFormat("dd MMM yyyy");
-                        Date date = fmt.parse(ent.Args);
-                        switch (stage2Type.toLowerCase()) {
-                            case "birt":
-                                workingPerson.SetBirthday(date);
-                                break;
-                            case "deat":
-                                workingPerson.SetDeathday(date);
-                                break;
-                            case "marr":
-                                workingFamily.SetMarrageDate(date);
-                                break;
-                            case "div":
-                                workingFamily.DivorceDate = date;
-                                break;
-                        
-                            default:
-                                break;
-                        }
-                        break;
+
                     default:
                         break;
+                    }
+                    break;
+                default:
+                    break;
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 gedErrors.add(new GEDError("Error with line " + ent.LineNumber + ": " + e.getMessage()));
             }
         }
-        System.out.println(String.format("|%6s|%20s|%5s|%15s|%6s|%6s|%15s|%8s|%8s|",
-        "ID","Name","Gender","Birth","Age","Alive","Death", "Child", "Spouse"));
+        System.out.println(String.format("|%6s|%20s|%5s|%15s|%6s|%6s|%15s|%8s|%8s|", "ID", "Name", "Gender", "Birth",
+                "Age", "Alive", "Death", "Child", "Spouse"));
         for (Person person : people.values()) {
             System.out.println(person.toString());
+            if (person.IsTooOld()) {
+                gedErrors.add(new GEDError("Error: Person with ID " + person.ID + " is older than 150 years"));
+            }
         }
 
-        System.out.println(String.format("|%6s|%15s|%15s|%7s|%20s|%7s|%20s|%15s|"
-        , "ID", "Marrage", "Divorce", "Husband", "Husband Name", "Wife", "Wife Name", "Children"));
+        System.out.println(String.format("|%6s|%15s|%15s|%7s|%20s|%7s|%20s|%15s|", "ID", "Marrage", "Divorce",
+                "Husband", "Husband Name", "Wife", "Wife Name", "Children"));
         for (Family family : families.values()) {
             System.out.println(family.Print(people));
         }
@@ -155,8 +148,7 @@ public class Main {
         }
     }
 
-    private static boolean IsValid(String tag, String[] tagList)
-    {
+    private static boolean IsValid(String tag, String[] tagList) {
         for (String validTag : tagList) {
             if (validTag.equalsIgnoreCase(tag)) {
                 return true;
--- src/Person.java
+++ src/Person.java
@@ -2,8 +2,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;;
 
-public class Person
-{
+public class Person {
     public String ID;
     public String Name;
     public String Gender;
@@ -12,64 +11,60 @@ public class Person
     public String Child;
     public String Spouse;
 
-    public Person()
-    {
+    public Person() {
         Child = "N/A";
         Spouse = "N/A";
     }
 
-    public long GetAge()
-    {
-        if (Birthday==null) {
-            return 0;
+    public long GetAge() {
+        if (Birthday == null) {
+            return -1;
         }
-        
+
         Date today = new Date();
-        if (DeathDay != null && DeathDay.compareTo(today)<0) {
+        if (DeathDay != null && DeathDay.compareTo(today) < 0) {
             today = DeathDay;
         }
         long diff = today.getTime() - Birthday.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) / 365;
     }
 
-    public Date GetBirthday()
-    {
+    public Date GetBirthday() {
         return Birthday;
     }
 
-    public void SetBirthday(Date date) throws Exception
-    {
-        if (DeathDay!=null&&DeathDay.compareTo(date)<0)
-        {
+    public void SetBirthday(Date date) throws Exception {
+        if (DeathDay != null && DeathDay.compareTo(date) < 0) {
             throw new Exception("Person can't be born after death.");
         }
         Birthday = date;
+
     }
 
-    public Date GetDeathday()
-    {
+    public Date GetDeathday() {
         return DeathDay;
     }
 
-    public void SetDeathday(Date date) throws Exception
-    {
-        if (Birthday!=null&&Birthday.compareTo(date)>0)
-        {
+    public void SetDeathday(Date date) throws Exception {
+        if (Birthday != null && Birthday.compareTo(date) > 0) {
             throw new Exception("Person can't die before birth.");
         }
         DeathDay = date;
     }
 
-    public boolean IsAlive()
-    {
-        return DeathDay==null;
+    public boolean IsTooOld() {
+        long age = GetAge();
+        return age > 150;
     }
 
-    public String toString()
-    {
-        String birthString = Birthday==null?null:new SimpleDateFormat("dd-MMM-yyyy").format(Birthday);
-        String deathString = DeathDay==null?null:new SimpleDateFormat("dd-MMM-yyyy").format(DeathDay);
-        return String.format("|%6s|%20s|%5s|%15s|%6d|%6b|%15s|%8s|%8s|",
-            ID,Name,Gender,birthString,GetAge(),IsAlive(),deathString, Child, Spouse);
+    public boolean IsAlive() {
+        return DeathDay == null;
+    }
+
+    public String toString() {
+        String birthString = Birthday == null ? null : new SimpleDateFormat("dd-MMM-yyyy").format(Birthday);
+        String deathString = DeathDay == null ? null : new SimpleDateFormat("dd-MMM-yyyy").format(DeathDay);
+        return String.format("|%6s|%20s|%5s|%15s|%6d|%6b|%15s|%8s|%8s|", ID, Name, Gender, birthString, GetAge(),
+                IsAlive(), deathString, Child, Spouse);
     }
 }
